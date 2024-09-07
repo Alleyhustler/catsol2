39759769394b92cb007708ec566354fce38ddef6
Alleyhustler <153684384+Alleyhustler@users.noreply.github.com>
2024-09-07 14:42:28 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Add shaking effect to the body
+document.addEventListener('DOMContentLoaded', () => {
+    // Add a class to the body to trigger the shaking effect
+    document.body.classList.add('shake');
+});
